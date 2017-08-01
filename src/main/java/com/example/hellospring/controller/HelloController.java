e5a9a7e04cc68eedba74b167b374da3a65332ca5
Kate Boston <ktlnbstn@gmail.com>
2017-07-31 23:56:20 -0700

    Add languages to greeting.

--- src/main/java/com/example/hellospring/controller/HelloController.java
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -25,18 +25,46 @@ public class HelloController {
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     @ResponseBody
     public String helloForm(){
-        String html = "<form method='post'>" +
+        String html = "<center><form method='post'>" +
                 "<input type='text' name='name' />" +
+                "<select name='langs' >" +
+                "<option value='eg'>English</option>" +
+                "<option value='fr'>French</option>" +
+                "<option value='jp'>Japanese</option>" +
+                "<option value='sp'>Spanish</option>" +
+                "<option value='mc'>Morse Code</option>" +
+                "<option value='ar'>Armenian</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet Me!'/>" +
-                "</form>";
+                "</form></center>";
         return html;
     }
 
     @RequestMapping(value = "hello", method = RequestMethod.POST)
     @ResponseBody
-    public String helloPost(HttpServletRequest request){
+    public String createMessage(HttpServletRequest request){
         String name = request.getParameter("name");
-        return "Hello " + name;
+        String lang = request.getParameter("langs");
+
+        if (lang.equals("fr")) {
+            return "Bonjour " + name + "!";
+        }
+        if (lang.equals("jp")) {
+            return "Konnichiwa " + name + "!";
+        }
+        if (lang.equals("sp")) {
+            return "Hola " + name + "!";
+        }
+        if (lang.equals("mc")) {
+            return ".... . .-.. .-.. --- " + name + "!";
+        }
+        if (lang.equals("ar")) {
+            return "Barev " + name + "!";
+        }
+        if (lang.equals("eg")) {
+            return "Hello " + name + "!";
+        }
+        return name;
     }
 
     @RequestMapping(value = "hello/{name}")
